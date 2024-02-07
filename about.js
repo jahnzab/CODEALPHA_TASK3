b30c30ab9168697a73e4784431b8cdd8825d9ec5
jahnzab <157870830+jahnzab@users.noreply.github.com>
2024-02-08 01:01:30 +0530

    Update about.js

--- about.js
+++ about.js
@@ -1,13 +1,7 @@
 
 
     // Retrieve the username from local storage
-    var username = localStorage.getItem("username");
-
-    // Check if username exists and update the DOM
-    if (username) {
-        document.getElementById("username").textContent = "Welcome, " + username + "!";
-    }
-
+  
 // SIDEBAR TOGGLE
 var sidebarOpen = false;
 var sidebar = document.getElementById('sidebar');
